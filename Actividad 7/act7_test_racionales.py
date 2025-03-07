2452b94f1ccd5221f2876d472234e82d511e773d
Sebastianpg1 <sebastianpgkm@gmail.com>
2025-03-07 17:57:03 -0600

    Actividad 7
    Act7

---
+++ Actividad 7/act7_test_racionales.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  7 17:46:09 2025
+
+@author: sebas
+"""
+
+import unittest
+import Act7_1_numeros
+
+class TestRacionales(unittest.TestCase):
+    #crear clase
+    def test_print_hello (self):
+        self.asselfEqual(, 'FOO')
+if __name__ == '__main__':
+    unittest.main()
+
+def print_hello(self):
+    return "Hello Angel Sebastian"
